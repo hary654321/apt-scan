966b2ec105df877948ca8a31bab2111c17975b47
wangrui <hary654321@126.com>
2023-07-15 11:14:47 +0800

    portscan

--- go.mod
+++ go.mod
@@ -6,6 +6,8 @@ require (
 	github.com/BurntSushi/toml v0.3.1
 	github.com/gin-gonic/gin v1.7.4
 	github.com/go-playground/validator/v10 v10.4.1
+	github.com/hary654321/gonmap v0.0.0-20230117075752-514409cd7d07
+	github.com/lcvvvv/stdio v0.1.2
 	github.com/shirou/gopsutil v3.21.11+incompatible
 	github.com/shirou/gopsutil/v3 v3.23.6
 	github.com/unrolled/secure v1.0.9
@@ -23,6 +25,7 @@ require (
 	github.com/leodido/go-urn v1.2.0 // indirect
 	github.com/lufia/plan9stats v0.0.0-20211012122336-39d0f177ccd0 // indirect
 	github.com/mattn/go-isatty v0.0.12 // indirect
+	github.com/miekg/dns v1.1.50 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v0.0.0-20180701023420-4b7aa43c6742 // indirect
 	github.com/niemeyer/pretty v0.0.0-20200227124842-a10e7caefd8e // indirect
@@ -33,7 +36,12 @@ require (
 	github.com/ugorji/go/codec v1.1.7 // indirect
 	github.com/yusufpapurcu/wmi v1.2.3 // indirect
 	golang.org/x/crypto v0.0.0-20200622213623-75b288015ac9 // indirect
+	golang.org/x/mod v0.4.2 // indirect
+	golang.org/x/net v0.0.0-20210726213435-c6fcb2dbf985 // indirect
 	golang.org/x/sys v0.9.0 // indirect
+	golang.org/x/text v0.3.7 // indirect
+	golang.org/x/tools v0.1.6-0.20210726203631-07bc1bf47fb2 // indirect
+	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
 	google.golang.org/protobuf v1.26.0 // indirect
 	gopkg.in/check.v1 v1.0.0-20200227125254-8fa46927fb4f // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
---
+++ model/port.go
@@ -0,0 +1,69 @@
+package model
+
+import (
+	"fmt"
+	"ias_tool_v2/core/scanner"
+	"time"
+
+	"github.com/go-playground/validator/v10"
+)
+
+type PortTask struct {
+	Task
+	ChReq       chan ReqParams `json:"request_tcp_param"` //待执行tcp参数的channel
+	Payloads    []Port         `json:"payload,omitempty"` //报文信息
+	Timeout     int            `json:"timeout"`           //读超时配置
+	ChMaxThread chan struct{}
+}
+
+type Port struct {
+	PortName string  `json:"Port_name" validate:"required,required"`
+	Payload  *string `json:"payload" validate:"required,required"`
+}
+
+type PortReqParam struct {
+	TaskId    string   `json:"task_id" validate:"required"`                //task id
+	ScanAddrs []string `json:"addrs" validate:"required,dive,required"`    //待扫描地址列表
+	Timeout   int      `json:"timeout,omitempty" validate:"required"`      //发起请求读超时
+	Threads   int      `json:"threads" validate:"min=1,max=8192,required"` //最大执行goroutine数量
+}
+
+// IsValid 校验参数
+func (request *PortReqParam) IsValid() (err error) {
+
+	validate := validator.New()
+	if err = validate.Struct(request); err != nil {
+		fmt.Println("字段校验出错", err.Error())
+		return err
+	}
+	return nil
+}
+
+type Config struct {
+	DeepInspection bool
+	Timeout        time.Duration
+	Threads        int
+	Interval       time.Duration
+}
+
+func DefaultConfig() *Config {
+	return &Config{
+		DeepInspection: false,
+		Timeout:        time.Second * 3,
+		Threads:        800,
+		Interval:       time.Millisecond * 300,
+	}
+}
+
+// NewPortTask 根据用户请求的参数生成Port任务
+// tlsport 如果 addrs 元素port部分存在在tlsport下，则执行https逻辑
+
+var PortScanner *scanner.PortClient
+
+func NewPortTask(p *PortReqParam) *scanner.PortClient {
+	PortConfig := scanner.DefaultConfig()
+	PortConfig.Threads = p.Threads
+	PortConfig.Timeout = time.Duration(p.Timeout) * time.Second // getTimeout(len(app.Setting.Port))
+	PortScanner = scanner.NewPortScanner(PortConfig)
+	return PortScanner
+}
