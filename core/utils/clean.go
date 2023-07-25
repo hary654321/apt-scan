b1bd770df344109dcde1effbb9d2754033e208a4
wangrui <hary654321@126.com>
2023-07-25 16:09:09 +0800

    操作系统

--- core/utils/clean.go
+++ core/utils/clean.go
@@ -3,7 +3,6 @@ package utils
 import "strings"
 
 var OsMap map[string]string = map[string]string{
-	"UOS":               "统信UOS",
 	"UOS_Desktop_104":   "统信UOS",
 	"(Uos)":             "统信UOS",
 	"AQUOS":             "统信UOS",
@@ -11,6 +10,11 @@ var OsMap map[string]string = map[string]string{
 	"Set-Cookie: AIROS": "Ubiquiti AirOS路由器",
 	"ciscoSystems":      "Cisco SNMP service",
 	"openeuler":         "openEuler OS",
+	"Deepin/":           "深度操作系统",
+	"(Deepin)":          "深度操作系统",
+	"(NeoKylin":         "中标麒麟操作系统",
+	"(Kylin)":           "银河麒麟Kylin OS",
+	"Asianux":           "红旗Asianux操作系统",
 }
 
 func Dealdata(m map[string]string) map[string]string {
